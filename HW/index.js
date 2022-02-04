b1fcfa8bf386418dfe1033fa0fde9b12a16c8614
PeachyMiichy <michellevong97@gmail.com>
2022-02-04 22:00:42 +0800

    Update of current weather icon
    Weather icon updates depending on weather of location

--- HW/index.js
+++ HW/index.js
@@ -50,6 +50,7 @@ function showWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#tempIcon");
 
   celsiusTemperature = response.data.main.temp;
   console.log(celsiusTemperature);
@@ -59,6 +60,11 @@ function showWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(event) {
